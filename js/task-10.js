e8fbc07f1cb73f30b574325ebdde57c8aff331ae
AnnaNesterenko <nura_arh@ukr.net>
2022-05-13 23:05:33 +0300

    finish

--- js/task-10.js
+++ js/task-10.js
@@ -1,37 +1,37 @@
 // Не работает. Подумаю позже
-// const input = document.querySelector("#controls input");
-// const create = document.querySelector("[data-create]");
-// const destroy = document.querySelector("[data-destroy]");
-// const boxes = document.querySelector("#boxes");
+const input = document.querySelector("#controls input");
+const create = document.querySelector("[data-create]");
+const destroy = document.querySelector("[data-destroy]");
+const boxes = document.querySelector("#boxes");
 
-// const inputChange = (event) => {
-//   input.setAttribute("count", Number(event.currentTarget.value));
-//   console.log(Number(event.currentTarget.value));
-// };
+const inputChange = (event) => {
+  input.setAttribute("count", Number(event.currentTarget.value));
+  console.log(Number(event.currentTarget.value));
+};
 
-// input.addEventListener("input", inputChange);
+input.addEventListener("input", inputChange);
 
-// create.addEventListener("click", onCreateBoxes);
-// destroy.addEventListener("click", onDestroyBoxes);
+create.addEventListener("click", onCreateBoxes);
+destroy.addEventListener("click", onDestroyBoxes);
 
-// let baseBoxSize = 30;
+let baseBoxSize = 30;
 
-// function onCreateBoxes() {
-//   let amount = Number(input.getAttribute("count"));
-//   for (let i = 0; i < amount; i += 1) {
-//     baseBoxSize += 10;
-//     const newBox = document.createElement("div");
-//     newBox.style.background = getRandomHexColor();
-//     newBox.style.height = baseBoxSize + "px";
-//     newBox.style.width = baseBoxSize + "px";
-//     boxes.append(newBox);
-//   }
-// }
+function onCreateBoxes() {
+  let amount = Number(input.getAttribute("count"));
+  for (let i = 0; i < amount; i += 1) {
+    baseBoxSize += 10;
+    const newBox = document.createElement("div");
+    newBox.style.background = getRandomHexColor();
+    newBox.style.height = baseBoxSize + "px";
+    newBox.style.width = baseBoxSize + "px";
+    boxes.append(newBox);
+  }
+}
 
-// function onDestroyBoxes() {
-//   boxes.innerHTML === "";
-// }
+function onDestroyBoxes() {
+  boxes.remove();
+}
 
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-// }
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
